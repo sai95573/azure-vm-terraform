6e74b2b9050913718148d1cc102b83a48c2ab270
sai95573 <sai95573@gmail.com>
2026-08-11 22:50:29 +0530

    updated fmt

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "testingazurestaging54321"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                          = "testingazurestaging54321"
+  resource_group_name           = azurerm_resource_group.example.name
+  location                      = azurerm_resource_group.example.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
   public_network_access_enabled = true
 
   blob_properties {
